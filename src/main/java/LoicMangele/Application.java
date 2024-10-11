120dce099c38f5e5fe7e85e58a7573cc93d655d7
sudonona <80674485+sudonona@users.noreply.github.com>
2024-10-11 08:46:53 +0200

    Initial commit

---
+++ src/main/java/LoicMangele/Application.java
@@ -0,0 +1,12 @@
+package LoicMangele;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("ProgettoSettimanale3");
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
